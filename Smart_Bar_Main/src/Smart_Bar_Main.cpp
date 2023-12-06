fbebadde1322f09a1b740de542b98bffbd5030b3
Rudy Parra <rparra25@cnm.edu>
2023-12-05 20:00:22 -0700

    Relay activating on its own before clicking

--- Smart_Bar_Main/src/Smart_Bar_Main.cpp
+++ Smart_Bar_Main/src/Smart_Bar_Main.cpp
@@ -31,6 +31,12 @@
     int numPixels = 16;
     Adafruit_NeoPixel pixel(numPixels, SPI1, WS2812B); // Use D6 for NeoPixels (SPI1)
 
+  // Setting Pump Ints
+    int pump1 = D19;
+    int pump2 = D14;
+    int pump3 = D13;
+    int pump4 = D12;
+  
   //Setting Encoder PINS
     int PINA = D17;
     int PINB = D18;
@@ -86,42 +92,47 @@ void condimentDispenser();
     pinMode(REDLED, OUTPUT);
     pinMode(BLUELED, OUTPUT);
 
+    //Pump Setup
+    pinMode(pump1, OUTPUT);
+    pinMode(pump2, OUTPUT);
+    pinMode(pump3, OUTPUT);
+    pinMode(pump4, OUTPUT);
+
+
   }
 
   void loop() {
+    digitalWrite(pump1, HIGH); //Testing Pump
+    digitalWrite(pump2, HIGH); //Testing Pump
+    digitalWrite(pump3, HIGH); //Testing Pump
+    digitalWrite(pump4, HIGH); //Testing Pump
     cocktailSelector();
     cocktailDispenser();
-    //condimentDispenser();
   }
 
   void cocktailSelector(){
     // Decarling Integers
     static int prevEnc;
     static int position;
-    int pump1 = D14;
     int i;
+    int j;
 
     // Decalring Time Parameters
     DateTime = Time.timeStr();    //Current Date and Time from Particle Time Class
     TimeOnly = DateTime.substring(11,19); //Extract the time from the DateTime String
     DateOnly = DateTime.substring(0,10); //Extracts the date from the DateTIme
 
-    // Setting the Encoder
+    // Setting the Encoder Default Light
     digitalWrite(GREENLED, HIGH); // Set the green LED to ON
     digitalWrite(REDLED, LOW);  // Set the red LED to OFF
     digitalWrite(BLUELED, LOW);  // Set the red LED to OFF
-    delay(1000);
 
     //Setting NeoPixel Status Ring
-    for (i = 0; i<numPixels; i++) {
-      pixel.setPixelColor(i, 255, 255, 255);
+    //for (i = 0; i<numPixels; i++) {
+      pixel.setPixelColor(j, 255, 255, 255);
       pixel.setBrightness(30);
       pixel.show();
-      pixel.clear();
-    }
-
-    //Testing Pump
-    digitalWrite(pump1, HIGH);
+    //}
   
     position = myEnc.read(); // Reading Encoder Position
     Serial.printf("position: %i\n",position);// Print Position on Serial Monitor
@@ -134,13 +145,25 @@ void condimentDispenser();
         display.printf("Margarita");
         display.setCursor(0, 25);
         display.display();
-          if ((position == 4) && (button.isClicked())) {
-            // If the button is pressed (NeoPixels ON)
-            digitalWrite(GREENLED, HIGH); // Set the green LED to ON
-            digitalWrite(REDLED, HIGH);  // Set the red LED to OFF
-            digitalWrite(BLUELED, HIGH);
+          if ((position == 4) && (button.isClicked())){
+            digitalWrite(GREENLED, HIGH);
+            digitalWrite(REDLED, HIGH);  
+            digitalWrite(BLUELED, LOW);
             Serial.printf("Button Clicked on \n");
             delay(1000);
+
+            //cocktailDispenser();
+
+            digitalWrite(pump1, HIGH); //Testing Pump
+            delay(2000);
+            digitalWrite(pump1, LOW); //Testing Pump
+
+            condimentDispenser();
+            digitalWrite(GREENLED, LOW); 
+            digitalWrite(REDLED, HIGH);  
+            digitalWrite(BLUELED, HIGH);
+            delay(1000);
+
           } 
         break;
       case 8:
@@ -148,48 +171,108 @@ void condimentDispenser();
         display.printf("Tequila \nSunrise");
         display.setCursor(0, 25);
         display.display();
+          if ((position == 8) && (button.isClicked())){
+              digitalWrite(GREENLED, HIGH);
+              digitalWrite(REDLED, HIGH);  
+              digitalWrite(BLUELED, LOW);
+              Serial.printf("Button Clicked on \n");
+              delay(1000);
+
+              digitalWrite(pump2, HIGH); //Testing Pump
+              delay(2000);
+              digitalWrite(pump2, LOW); //Testing Pump
+
+              condimentDispenser();
+              digitalWrite(GREENLED, LOW); 
+              digitalWrite(REDLED, HIGH);  
+              digitalWrite(BLUELED, HIGH);
+              delay(1000);
+          }
         break;
       case 12:
         display.clearDisplay();
         display.printf("Screw \nDriver");
         display.setCursor(0, 25);
         display.display();
+          if ((position == 12) && (button.isClicked())){
+            digitalWrite(GREENLED, HIGH);
+            digitalWrite(REDLED, HIGH);  
+            digitalWrite(BLUELED, LOW);
+            Serial.printf("Button Clicked on \n");
+            delay(1000);
+
+            digitalWrite(pump3, HIGH); //Testing Pump
+            delay(2000);
+            digitalWrite(pump3, LOW); //Testing Pump
+
+            condimentDispenser();
+            digitalWrite(GREENLED, LOW); 
+            digitalWrite(REDLED, HIGH);  
+            digitalWrite(BLUELED, HIGH);
+            delay(1000);
+          }
         break;
       case 16:
         display.clearDisplay();
         display.printf("Vodka \nCranberry");
         display.setCursor(0, 25);
         display.display();
+          if ((position == 16) && (button.isClicked())){
+            digitalWrite(GREENLED, HIGH);
+            digitalWrite(REDLED, HIGH);  
+            digitalWrite(BLUELED, LOW);
+            Serial.printf("Button Clicked on \n");
+            delay(1000);
+
+            digitalWrite(pump4, HIGH); //Testing Pump
+            delay(2000);
+            digitalWrite(pump4, LOW); //Testing Pump
+
+
+            condimentDispenser();
+            digitalWrite(GREENLED, LOW); 
+            digitalWrite(REDLED, HIGH);  
+            digitalWrite(BLUELED, HIGH);
+            delay(1000);
+          }
         break;
-      // Add more cases as needed
       default:
         display.clearDisplay();
         display.printf("%s\n%s",DateOnly.c_str(), TimeOnly.c_str());
         display.setCursor(0, 15);
         display.display();
       }
-    //}
+  //}
   }
+
   void cocktailDispenser() {
-    // Setting Pump Variables and Parameters
-    /* int tequilaPump = D12;
-    int vodkaPump = D13;
-    int mixer1Pump = D14;
-    int mixer2Pump = D19;
+    // Setting Pump Ints
+    int pump1 = D19;
 
-    digitalWrite(tequilaPump ,HIGH);
-    delay(500);
-    digitalWrite(tequilaPump,LOW); */
+    // Setting Scale Ints
+    unsigned int last;
+    int DT = D2;
+    int CLK = D0;
 
+      // Activating Pump
+      digitalWrite(pump1, HIGH); //Testing Pump
+      delay(2000);
+      digitalWrite(pump1, LOW); //Testing Pump
 
+      // Activating Condiment Dispenser
+      condimentDispenser();
 
+      // Activating Encoder Status LEDs
+      digitalWrite(GREENLED, LOW); 
+      digitalWrite(REDLED, HIGH);  
+      digitalWrite(BLUELED, HIGH);
+      delay(1000);
   }
 
   void condimentDispenser(){
       // Setting Steppers Parameters and Variables
       int speed = 15;
-      int twoRotation = 4096;
-      int oneCounterRotation = -4096;
+      int twoRotation = -4096;
       int spr = 2048;
       int IN1 = D3;
       int IN2 = D4;
@@ -200,6 +283,4 @@ void condimentDispenser();
 
       // Running the Stepper Motor
       myStepper.step(twoRotation);
-      /* delay(1000);
-      myStepper.step(oneCounterRotation); */
   }
